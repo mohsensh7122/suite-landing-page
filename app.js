f1d1b4d195a3dd8ecdaaf99293883d8a244d87a8
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-04-20 17:27:36 -0400

    Fixed the hero component card for tablets

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 let iphonePic = document.querySelector('.iphone-pic');
 
-function myFunction(x) {
+
+function myFunction(x, y) {
     if (x.matches) { // If media query matches
         iphonePic.src = "assets/image-hero-portrait.png"
+    } else if (y.matches){
+        iphonePic.src = "assets/image-hero-portrait@2x.png"
     } else {
-        iphonePic.src = "assets/image-hero-portrait.png"
+        iphonePic.src = "assets/image-hero-landscape.png"
     }
   }
   
-  var x = window.matchMedia("(max-width: 768px)")
-  myFunction(x) // Call listener function at run time
+  var x = window.matchMedia("(min-width: 768px)")
+  var y = window.matchMedia("(min-width: 1024px)")
+  myFunction(x, y) // Call listener function at run time
   x.addListener(myFunction) // Attach listener function on state changes
+  y.addListener(myFunction) // Attach listener function on state changes
